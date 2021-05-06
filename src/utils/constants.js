dd0ee5cd0a45c40d805e1b3a202390104fd6b46a
BurakCaglar <burakcaglar@yandex.com>
2021-05-06 03:01:45 +0300

    constants added

---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+export const NavContent = [
+  { id: 1, name: "HOME", url: "/images/home-icon.svg" },
+  { id: 2, name: "SEARCH", url: "/images/search-icon.svg" },
+  { id: 3, name: "WATCHLIST", url: "/images/watchlist-icon.svg" },
+  { id: 4, name: "ORIGINALS", url: "/images/original-icon.svg" },
+  { id: 5, name: "MOVIES", url: "/images/movie-icon.svg" },
+  { id: 5, name: "SERIES", url: "/images/series-icon.svg" },
+];
