655c0ce52287f675b7c4ccd41ad27d51d247eebb
BurakCaglar <burakcaglar@yandex.com>
2021-05-06 17:35:05 +0300

    ADD: Viewers

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,3 +6,11 @@ export const NavContent = [
   { id: 5, name: "MOVIES", url: "/images/movie-icon.svg" },
   { id: 5, name: "SERIES", url: "/images/series-icon.svg" },
 ];
+
+export const ViewersContent = [
+  { id: 1, url: "/images/viewers-disney.png" },
+  { id: 2, url: "/images/viewers-pixar.png" },
+  { id: 3, url: "/images/viewers-marvel.png" },
+  { id: 4, url: "/images/viewers-starwars.png" },
+  { id: 5, url: "/images/viewers-national.png" },
+];
